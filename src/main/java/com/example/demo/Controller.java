4014a3af78340360be73b6a6b19d403b43288271
TomBartie <63671640+TomBartie@users.noreply.github.com>
2023-03-16 23:28:29 -0400

    Spring Boot Created
    Created base spring boot back end.  We will also use the Controller class in the main folder for our API using Spring MVC.

---
+++ src/main/java/com/example/demo/Controller.java
@@ -0,0 +1,7 @@
+package com.example.demo;
+
+/* This will be our REST API - Uses Spring MVC so   
+*  we will be using MVC as an architectural pattern*/
+public class Controller {
+
+}
---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	// Build out main class as a Bridge design pattern
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}

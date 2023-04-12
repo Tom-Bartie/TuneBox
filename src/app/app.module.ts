f0aee7f89979c1cf95562fb7ac7244d1d9d4c737
Derek Park <79765656+derekepark@users.noreply.github.com>
2023-04-12 18:50:47 -0400

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { PopularReviewsComponent } from './popular-reviews/popular-reviews.component';
+import { FirstRowComponent } from './first-row/first-row.component';
+import { NotFirstRowComponent } from './not-first-row/not-first-row.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    NavigationBarComponent,
+    PopularReviewsComponent,
+    FirstRowComponent,
+    NotFirstRowComponent
   ],
   imports: [
     BrowserModule,

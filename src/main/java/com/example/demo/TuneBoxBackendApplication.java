2c36c6dd046b5e139f5977c0293e5555e8767a48
TomBartie <63671640+TomBartie@users.noreply.github.com>
2023-04-13 13:25:44 -0400

    Connected to database

--- src/main/java/com/example/demo/TuneBoxBackendApplication.java
+++ src/main/java/com/example/demo/TuneBoxBackendApplication.java
@@ -4,8 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
 
-import com.example.demo.model.User;
 import com.example.demo.repository.UserRepository;
 
 /* NOTE: Here are some links on how I am implementing the backend
@@ -23,9 +23,15 @@ public class TuneBoxBackendApplication implements CommandLineRunner {
 	@Autowired
 	private UserRepository userRepository;
 
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
 	@Override
 	public void run(String... args) throws Exception {
-		this.userRepository.save(new User("tom-bartie", "password123", "tjb141@pitt.edu"));
+		// this.userRepository.save(new User("tom-bartie", "password123",
+		// "tjb141@pitt.edu"));
+		String sql = "INSERT INTO PROFILE (UserID, UserName, Password, Email) VALUES ('1','tom-bartie', 'password123', 'tjb141@pitt.edu')";
+		int rows = jdbcTemplate.update(sql);
 	}
 
 }

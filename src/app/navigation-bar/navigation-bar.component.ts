97b865ca4a5d8dc8f84c5489ee7831dfd8afc290
gregdrew12 <gregdrew12@gmail.com>
2023-04-16 21:11:29 -0400

    TUNEBOX logo routing to home page

--- src/app/navigation-bar/navigation-bar.component.ts
+++ src/app/navigation-bar/navigation-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navigation-bar',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./navigation-bar.component.scss']
 })
 export class NavigationBarComponent {
+  constructor(private route: Router){ }
 
+  navigateToHome(){
+    this.route.navigate(['Home'])
+  }
 }
